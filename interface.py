4301512811f48448559ff5ac4dae97cd7c9e8c68
Аминев Денис Салаватович <daminev@skbkontur.ru>
2018-04-07 20:08:33 +0500

    library done!

--- interface.py
+++ interface.py
@@ -4,14 +4,34 @@ import Library
 def game(text):
     if text.lower() == "Библиотека".lower():
         return Library.description()
-    else:
-        return "Ну е мае"
+    elif text.lower() == "Подойти к человеку".lower():
+        return Library.dialog_start()
+    elif text.lower() == "Я расследую исчезновение лорда. А ты случайно не Ри?".lower():
+        return Library.dialog_1()
+    elif text.lower() == "Эти байки для стражи можешь и оставить, а мне расскажи-ка лучше про это *кладете перед ним книгу в черной обложке*".lower():
+        return Library.dialog_2()
+    elif text.lower() == "Тише-тише, я даже не понимаю, что здесь написано.".lower():
+        return Library.dialog_end()
+    elif text.lower() == "Хм.. ну ладно".lower():
+        return Library.end_room()
 
 
 def keyboard(text):
     if text.lower() == "Библиотека".lower():
-       keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="%s" % Library.initiate())]])
-       return keyboard
-
-
-
+       keyboard_show = Library.initiate()
+       return keyboard_show
+    elif text.lower() == "Подойти к человеку".lower():
+        keyboard_show = Library.dialog_answer_buttons_1()
+        return keyboard_show
+    elif text.lower() == "Я расследую исчезновение лорда. А ты случайно не Ри?".lower():
+        keyboard_show = Library.dialog_answer_buttons_2()
+        return keyboard_show
+    elif text.lower() == "Эти байки для стражи можешь и оставить, а мне расскажи-ка лучше про это *кладете перед ним книгу в черной обложке*".lower():
+        keyboard_show = Library.dialog_answer_buttons_3()
+        return keyboard_show
+    elif text.lower() == "Тише-тише, я даже не понимаю, что здесь написано.".lower():
+        keyboard_show = Library.dialog_end_buttons()
+        return keyboard_show
+    elif text.lower() == "Хм.. ну ладно".lower():
+        keyboard_show = Library.end_room_buttons()
+        return keyboard_show
