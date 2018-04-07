2ee080ae9f72f24d8b402f490a2b423e0786dbd5
Аминев Денис Салаватович <daminev@skbkontur.ru>
2018-04-07 19:25:15 +0500

    lower()

--- interface.py
+++ interface.py
@@ -2,14 +2,14 @@ from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboar
 import Library
 
 def game(text):
-    if text == "Библиотека":
+    if text.lower() == "Библиотека".lower():
         return Library.description()
     else:
         return "Ну е мае"
 
 
 def keyboard(text):
-    if text == "Библиотека":
+    if text.lower() == "Библиотека".lower():
        keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="%s" % Library.initiate())]])
        return keyboard
 
